f1ca72f40f2befb09628803d9b9906cbc4572d68
RBrignoli <ramon.brignoli@gmail.com>
2025-02-20 09:05:21 -0300

    starting basic MVC setup

---
+++ controllers/users.js
@@ -0,0 +1,26 @@
+
+const list_users = async (req,res) => {
+    console.log('ok')
+    res.status(200).send({'users': true})
+}
+
+const get_user = async (req,res) => {
+    res.status(200).send({'users': true})
+}
+
+
+const delete_user = async (req,res) => {
+    res.status(200).send({'users': true})
+}
+
+
+const create_user = async (req,res) => {
+    res.status(200).send({'users': true})
+}
+
+const change_image = async (req,res) => {
+    res.status(200).send({'users': true})
+}
+
+
+export {list_users,get_user,delete_user,change_image,create_user}
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//start
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+import user_routes from './routes/users.js'
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/user', user_routes)
+
+app.listen(8000)
